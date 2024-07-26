2ed0b3552b03d9e6b36ebc04c8dd69dec148072f
OtegaClinton <oghenetegaokotie@gmail.com>
2024-07-26 21:36:04 +0100

    third commit

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+require("dotenv").config();
 const express = require('express')
+const session = require("express-session");
+const MongoDBStore = require('connect-mongodb-session')(session);
 const app = express()
 const port = 2000
 app.use(express.json())
 const db = require('./config/db')
 const router = require('./routers/schoolRouter')
 
-app.get("/",(req,res)=>{
-    res.send(`welcome to my first hosting`)
-})
+// app.get("/",(req,res)=>{
+//     res.send(`welcome to my first hosting`)
+// });
+
 app.use(router)
+
+const store = new MongoDBStore({
+    uri: process.env.databaseUrl,
+    collection: 'mySessions'
+  });
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }));
+
+  app.get("/",(req,res)=>{
+    req.session.isAuth = true;
+    console.log(req.session);
+    res.send("Welcome to session auth")
+  })
 
 
 app.listen(port, ()=>{
